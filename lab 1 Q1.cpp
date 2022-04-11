1da4a92da344b6f8f327d50803091c3d46ff01a1
Hafiz Ali Haider <103423628+Empathizer@users.noreply.github.com>
2022-04-11 16:25:56 +0500

    Add files via upload

---
+++ lab 1 Q1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+    int a,b,sum,sub,div,multi,rem;
+	cout<<"input any two nummbers";
+	cin>>a;
+	cin>>b;
+	//addition
+	sum=a+b;
+	cout<<"sum of two numbers= "<<sum<<endl;
+	//subtraction
+	sub=a-b;
+	cout<<"subtration of two numbers= "<<sub<<endl;
+	//division
+	div=a/b;
+	cout<<"division of two numbers= "<<div<<endl;
+	//multiplication
+	multi=a*b;
+	cout<<"product of two numbers= "<<multi<<endl;
+	//remainder
+	rem=a%b;
+	cout<<"reminder of two numbers= "<<rem<<endl;
+	
+}
